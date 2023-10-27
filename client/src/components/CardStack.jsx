93d1f8a05d1bddbf6bd0329aee190422e7cf8e6b
greeneggsanddan <greeneggsanddan@gmail.com>
2023-10-26 17:23:31 -0700

    Lift the RankBar component

--- client/src/components/CardStack.jsx
+++ client/src/components/CardStack.jsx
@@ -1,13 +1,15 @@
-import { useState } from "react";
 import { DndContext, DragOverlay, pointerWithin } from "@dnd-kit/core";
-import Toolbar from "./Toolbar";
-import RankBar from "./RankBar";
 import DroppableZone from "./DroppableZone";
 import DraggableCard from "./DraggableCard";
 
-export default function CardStack({ cardData, setCardData, currentCard, setCurrentCard }) {
-  const [rank, setRank] = useState(cardData[currentCard].rank);
-
+export default function CardStack({
+  cardData,
+  setCardData,
+  currentCard,
+  setCurrentCard,
+  rank,
+  setRank
+}) {
   const card = cardData[currentCard];
 
   function handleDragEnd() {
@@ -19,8 +21,8 @@ export default function CardStack({ cardData, setCardData, currentCard, setCurre
       return c;
     });
     setCardData(updatedCards);
-    if (currentCard === cardData.length - 1) setCurrentCard(0)
-    else setCurrentCard(currentCard + 1)
+    if (currentCard === cardData.length - 1) setCurrentCard(0);
+    else setCurrentCard(currentCard + 1);
   }
 
   function handleDragOver(e) {
@@ -28,38 +30,20 @@ export default function CardStack({ cardData, setCardData, currentCard, setCurre
   }
 
   return (
-    <>
-      <div className="position-relative">
-        <DndContext
-          collisionDetection={pointerWithin}
-          onDragEnd={handleDragEnd}
-          onDragOver={handleDragOver}
-        >
-          <DraggableCard>
-            <img
-              src={card.image}
-              alt={card.name}
-              className="w-100"
-            />
-          </DraggableCard>
-          <DragOverlay>
-            <img
-              src={card.image}
-              alt={card.name}
-              className="w-100"
-            />
-          </DragOverlay>
-          <DroppableZone />
-        </DndContext>
-      </div>
-      <RankBar
-        cardData={cardData}
-        setCardData={setCardData}
-        rank={rank}
-        setRank={setRank}
-        currentCard={currentCard}
-        setCurrentCard={setCurrentCard}
-      />
-    </>
+    <div className="position-relative">
+      <DndContext
+        collisionDetection={pointerWithin}
+        onDragEnd={handleDragEnd}
+        onDragOver={handleDragOver}
+      >
+        <DraggableCard>
+          <img src={card.image} alt={card.name} className="w-100" />
+        </DraggableCard>
+        <DragOverlay>
+          <img src={card.image} alt={card.name} className="w-100" />
+        </DragOverlay>
+        <DroppableZone />
+      </DndContext>
+    </div>
   );
 }
--- client/src/components/Ranker.jsx
+++ client/src/components/Ranker.jsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
 import CardStack from "./CardStack";
+import RankBar from "./RankBar";
 import CardNav from "./CardNav";
 
 export default function Ranker({ cardData, setCardData, currentCard, setCurrentCard }) {
+  const [rank, setRank] = useState(cardData[currentCard].rank);
   
   return (
-    <div className="sticky-top" style={{ top: '1.5rem'}}>
+    <div className="sticky-top" style={{ top: "1.5rem" }}>
       <CardStack
         cardData={cardData}
         setCardData={setCardData}
+        currentCard={currentCard}
+        setCurrentCard={setCurrentCard}
+        rank={rank}
+        setRank={setRank}
+      />
+      <RankBar
+        cardData={cardData}
+        setCardData={setCardData}
+        rank={rank}
+        setRank={setRank}
         currentCard={currentCard}
         setCurrentCard={setCurrentCard}
       />
